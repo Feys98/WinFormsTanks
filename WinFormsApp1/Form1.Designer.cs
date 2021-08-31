bbca1d17ea6759eb4f0d147a02ab769627e120d0
Robert Szwed <robertszwed98@gmail.com>
2021-08-31 13:55:57 +0200

    Game 0.0.1

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,20 +29,100 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.wallBottom = new System.Windows.Forms.PictureBox();
+            this.wallTop = new System.Windows.Forms.PictureBox();
+            this.wallLeft = new System.Windows.Forms.PictureBox();
+            this.WallRight = new System.Windows.Forms.PictureBox();
+            this.tank = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.wallBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wallTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wallLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WallRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tank)).BeginInit();
             this.SuspendLayout();
+            // 
+            // wallBottom
+            // 
+            this.wallBottom.BackColor = System.Drawing.Color.DimGray;
+            this.wallBottom.Location = new System.Drawing.Point(28, 1081);
+            this.wallBottom.Name = "wallBottom";
+            this.wallBottom.Size = new System.Drawing.Size(1100, 40);
+            this.wallBottom.TabIndex = 0;
+            this.wallBottom.TabStop = false;
+            this.wallBottom.Tag = "wall";
+            // 
+            // wallTop
+            // 
+            this.wallTop.BackColor = System.Drawing.Color.DimGray;
+            this.wallTop.Location = new System.Drawing.Point(28, 21);
+            this.wallTop.Name = "wallTop";
+            this.wallTop.Size = new System.Drawing.Size(1100, 40);
+            this.wallTop.TabIndex = 0;
+            this.wallTop.TabStop = false;
+            this.wallTop.Tag = "wall";
+            // 
+            // wallLeft
+            // 
+            this.wallLeft.BackColor = System.Drawing.Color.DimGray;
+            this.wallLeft.Location = new System.Drawing.Point(28, 21);
+            this.wallLeft.Name = "wallLeft";
+            this.wallLeft.Size = new System.Drawing.Size(40, 1100);
+            this.wallLeft.TabIndex = 0;
+            this.wallLeft.TabStop = false;
+            this.wallLeft.Tag = "wall";
+            // 
+            // WallRight
+            // 
+            this.WallRight.BackColor = System.Drawing.Color.DimGray;
+            this.WallRight.Location = new System.Drawing.Point(1088, 21);
+            this.WallRight.Name = "WallRight";
+            this.WallRight.Size = new System.Drawing.Size(40, 1100);
+            this.WallRight.TabIndex = 0;
+            this.WallRight.TabStop = false;
+            this.WallRight.Tag = "wall";
+            // 
+            // tank
+            // 
+            this.tank.BackColor = System.Drawing.Color.Black;
+            this.tank.BackgroundImage = global::WinFormsApp1.Properties.Resources.TankModel;
+            this.tank.Location = new System.Drawing.Point(151, 935);
+            this.tank.Name = "tank";
+            this.tank.Size = new System.Drawing.Size(50, 75);
+            this.tank.TabIndex = 1;
+            this.tank.TabStop = false;
+            this.tank.Tag = "tank";
+            this.tank.Click += new System.EventHandler(this.tank_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1366, 835);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1178, 1144);
+            this.Controls.Add(this.tank);
+            this.Controls.Add(this.WallRight);
+            this.Controls.Add(this.wallLeft);
+            this.Controls.Add(this.wallTop);
+            this.Controls.Add(this.wallBottom);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wallBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wallTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wallLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WallRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tank)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox wallBottom;
+        private System.Windows.Forms.PictureBox wallTop;
+        private System.Windows.Forms.PictureBox wallLeft;
+        private System.Windows.Forms.PictureBox WallRight;
+        private System.Windows.Forms.PictureBox tank;
     }
 }
 
